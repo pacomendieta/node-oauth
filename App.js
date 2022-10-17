5c9ce7d5b8856865bdc1e4665ceef49474241e84
pacomendieta <ff.mendieta@gmail.com>
2022-10-17 14:14:50 +0200

    Auth Google ok. Pdte ver dstos sesion

--- App.js
+++ App.js
@@ -3,7 +3,7 @@
 const express= require('express');
 const passport= require('passport')
 const path= require('path')
-const googleapi = require('./googleapikey')
+const googleapi = require('./googleapikey') //clave de api y client id de Google
 const GoogleStrategy= require('passport-google-oauth20').Strategy
 
 
@@ -17,26 +17,29 @@ app.use(session( {
   resave: false
 }))
 
-// DATOS DE LA APP REGISTRADOS EN GOOGLE DEVELOPPER
-// client id is the parameter that we will get from the google developer console
+// DATOS DE LA APP REGISTRADOS EN GOOGLE DEVELOPPER CONSOLE
 CLIENT_ID=googleapi.client_id
 CLIENT_SECRET=googleapi.client_secret
 CALLBACK_URL=googleapi.redirect_uris[0]
 
-// port number 
-PORT=8081
+
+
 
 // configuring passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
 
-passport.serializeUser( function(id, done) {
-  done(null, id)
+passport.serializeUser( function(user, done) {
+  done(null, user)
 })
 
-passport.deserializeUser( function(id, done) {
-  done(null, id)
+passport.deserializeUser( function(user, done) {
+  done(null, user)
 })
+
+
+
+//console.log("GOOGLE API:", googleapi)
 
 // following middleware will run whenever passport. Authenticate method is called and returns different parameters defined in the .
 passport.use(new GoogleStrategy({
@@ -56,27 +59,34 @@ passport.use(new GoogleStrategy({
 ))
 
 
+
+
 // serving home page for the application
-app.get('/', (req, res)=>
-
-{
-  //res.send("PAGINA HOME")
-  res.sendFile(path.join(__dirname + '/public/index.html'))
-
+app.get('/', (req, res)=>{
+  res.render("index");  
+  //res.sendFile(path.join(__dirname + '/public/index.html'))
 })
 
 // PAGINA SUCCESS - LOGIN OK
 app.get('/success', (req, res)=>
 {
-  res.sendFile(path.join(__dirname + '/public/loginok.html'))
+  res.send(req.session);
+  //res.sendFile(path.join(__dirname + '/public/loginok.html'))
 })
 
 // user will be redirected to the google auth page whenever hits the ‘/google/auth’ route.
 app.get('/google/auth',
-  passport.authenticate('google', {scope: ['profile', 'email']})
+  passport.authenticate('google', {
+    //scope: ['profile', 'email','https://www.googleapis.com/auth/calendar']})
+    scope: [
+      'profile', 
+      'https://www.googleapis.com/auth/userinfo.email',
+      'https://www.googleapis.com/auth/calendar'
+    ]
+  })
 )
 
-// authentication failure redirection is defined in the following route
+//PAGINA DE CALLBACK
 app.get('/auth/google/callback',
   passport.authenticate('google', {failureRedirect: '/'}),
   (req, res)=>
@@ -86,7 +96,8 @@ app.get('/auth/google/callback',
 )
 
 // running server
-app.listen(PORT, ()=>
+app.listen(process.env.puerto, ()=>
 {
-  console.log("Server is running on Port " + PORT)
+  console.log("Server is running on Port " + process.env.puerto)
 })
+app.set('view engine', 'ejs');
